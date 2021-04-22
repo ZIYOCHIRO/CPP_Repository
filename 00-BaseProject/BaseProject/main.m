709f9407864827e80fb3c406c9f2fa633f13a51a
ziyo <ziyochiro@gmail.com>
2021-04-22 20:30:37 +0800

    first commit

---
+++ 00-BaseProject/BaseProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BaseProject
+//
+//  Created by ruixinyi on 2021/3/1.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
