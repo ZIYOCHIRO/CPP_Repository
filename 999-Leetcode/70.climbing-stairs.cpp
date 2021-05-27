bca6300911383b27128c592c1f6248e6c4fef9f0
ziyo <ziyochiro@gmail.com>
2021-05-27 19:44:54 +0800

    update leetcode

--- 999-Leetcode/70.climbing-stairs.cpp
+++ 999-Leetcode/70.climbing-stairs.cpp
@@ -8,8 +8,23 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        
+        if (n <= 2) return n;
+        int last = 0;
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            int tem = result;
+            result += last;
+            last = tem;
+        }
+        return result;
     }
+    
+    /*
+     爬30层阶梯的方法数f(30)
+     爬29层阶梯的方法数f(29) + 1层阶梯
+     爬28层阶梯的方法数f(28) + 2层阶梯
+     f(30) = f(29) + f(28)
+     */
 };
 // @lc code=end
 
